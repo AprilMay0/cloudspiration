7f8e494e08927c9cd9dd137ceaef0de5b48883cd
April-May <april-may.costandinidis@mrs-electronics.com>
2018-06-24 21:01:59 -0400

    added downloading

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,5 +1,6 @@
 import { DataService } from './../services/data.service';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'photo',
@@ -18,7 +19,6 @@ export class PhotoComponent implements OnInit {
 
   ngOnInit() {
     this.service.getText()
-      // .subscribe(texts => this.texts = texts);
       .subscribe(texts => {
         this.texts = texts;
         this.assignQuote(true)
@@ -33,16 +33,17 @@ export class PhotoComponent implements OnInit {
 
   createImage(quote) {
     let context: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
-    context.font = "30pt Calibri";
+    context.font = "30pt Trebuchet MS";
     context.textAlign = "center";
     context.textBaseline = 'middle';
     context.strokeStyle = "black";
     context.fillStyle = "white";
     context.lineWidth = 6;
     let quoteText = this.getLines(context, quote, 500);
-    let height = 300-((quoteText.length) * 15)-50;
-    
+    let height = 300 - ((quoteText.length) * 15) - 50;
+
     let base_image = new Image();
+    base_image.setAttribute('crossOrigin', 'anonymous');
     base_image.src = 'https://cloudspiration-e784.restdb.io/media/' + this.image;
     base_image.onload = function () {
       context.drawImage(base_image, 0, 0, 600, 600);
@@ -50,8 +51,7 @@ export class PhotoComponent implements OnInit {
         height += 50;
         context.strokeText(quoteText[a], 300, height);
         context.fillText(quoteText[a], 300, height);
-      }    
-      
+      }
     }
   }
 
@@ -84,6 +84,13 @@ export class PhotoComponent implements OnInit {
     }
     lines.push(currentLine);
     return lines;
+  }
+
+  download() {
+    let canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+    canvas.toBlob(function(blob) {
+      FileSaver.saveAs(blob, "cloudspiration.png");
+  });
   }
 
 }
